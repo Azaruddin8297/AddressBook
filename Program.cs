b07e3198b3d8f0b8b476f7b7c24442ee098d8dd2
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-22 12:59:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AdressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Adderess Book");
+
+                   
+        }
+    }
+}
