b1c2bf1599a14e87c700edf05fd78169eb689f57
Azaruddin8297 <ajarpopo.com>
2023-03-22 12:57:54 +0530

    Adress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AdressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Adderess Book");
+            Contacts add = new Contacts("ajaruddin", "shaik","17","Atmakur","A.P", "518422", "India","8297214219","ajarpopo@gmial.com");
+
+                   
+        }
+    }
+}
