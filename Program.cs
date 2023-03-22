f1f4a44fef389109d50c74d86064280f87d7671b
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-22 12:59:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcom to the Adderess Book");
-            Contacts add = new Contacts("ajaruddin", "shaik","17","Atmakur","A.P", "518422", "India","8297214219","ajarpopo@gmial.com");
 
                    
         }
