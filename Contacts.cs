e3125b673dccdf9b7972506184138af53ef9c5f0
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-23 12:43:37 +0530

    Update Contacts.cs

--- Contacts.cs
+++ Contacts.cs
@@ -7,38 +7,17 @@ using System.Threading.Tasks;
 
 namespace AdressBook
 {
-    public class Contacts
+      public class Contacts
     {
-        string firstName;
-        string lastName;
-        string address;
-        string city;
-        string state;
-        string zip;
-        string country;
-        string phone;
-        string email;
-
-        public Contacts(string firstName, string lastName, string address, string city, string state, string zip, string country, string phone, string email)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.country = country;
-            this.phone = phone;
-            this.email = email;
-            
-
-        }
-         public void Display()
-        {
-            Console.WriteLine("Name of the Person {0}{1}, Adress is :{2}, his from {3} city and {4} sate.pincode is {5} His from {6}", firstName, lastName, address, city, state, zip, country);
-            Console.WriteLine("His Number is {0} and emails is {1}", phone, email);
-        }
-       
-       
-    } 
+        public string firstname {  get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; } 
+        public string country { get; set; }
+        public string phoneNo { get; set; }
+        public string eMail { get; set; }
+        
+    }
 }
