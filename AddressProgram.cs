f37219b9c82adf2166c72fef38bc46e6754a15fe
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-23 16:57:16 +0530

    Update AddressProgram.cs

--- AddressProgram.cs
+++ AddressProgram.cs
@@ -8,13 +8,9 @@ namespace AddressBookMangement
 {
     public class AddressProgram
     {
-        List<Address> list = new List<Address>();
         public void details()
         {
-
-            Console.WriteLine("Enter no.of details want to print:");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            List<Address> list = new List<Address>();
 
             {
                 Address address = new Address();
@@ -40,56 +36,52 @@ namespace AddressBookMangement
                 address.eMail = Console.ReadLine();
                 list.Add(address);
                 Console.WriteLine(address.firstname + "\n" + address.lastname + "\n" + address.address + "\n" + address.city + "\n" + address.state + "\n" + address.phoneNo + "\n" + address.postcode + "\n" + address.country + "\n" + address.eMail);
-            }
-            Console.WriteLine("Edit by using First Name: ");
-            String name = Console.ReadLine();
-            foreach (var data in list)
-            {
-                Address address = new Address();
-                if (data.firstname == name)
+                Console.WriteLine("Edit by using First Name: ");
+                String name = Console.ReadLine();
+                foreach (var data in list)
                 {
-
-                    Console.WriteLine("Enter option to edit:\n1.First Name\n2.Second Name\n3.Address\n4.city\n5.state\n6.phone number\n7.email");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
+                    if (data.firstname == name)
                     {
-                        case 1:
-                            Console.WriteLine("Enter first name ");
-                            data.firstname = Console.ReadLine();
-                            break;
-                        case 2:
-                            Console.WriteLine("Enter last name ");
-                            data.lastname = Console.ReadLine();
-                            break;
-                        case 3:
-                            Console.WriteLine("Enter address");
-                            data.address = Console.ReadLine();
-                            break;
-                        case 4:
-                            Console.WriteLine("Enter city");
-                            data.city = Console.ReadLine();
-                            break;
-                        case 5:
-                            Console.WriteLine("Enter state");
-                            data.state = Console.ReadLine();
-                            break;
-                        case 6:
-                            Console.WriteLine("Enter phne number");
-                            data.phoneNo = Console.ReadLine();
-                            break;
-                        case 7:
-                            Console.WriteLine("Enter email");
-                            data.eMail = Console.ReadLine();
-                            break;
+                       Console.WriteLine("Enter option to edit:\n1.First Name\n2.Second Name\n3.Address\n4.city\n5.state\n6.phone number\n7.email");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter first name ");
+                                data.firstname = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter last name ");
+                                data.lastname = Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter address");
+                                data.address = Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter city");
+                                data.city = Console.ReadLine();
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter state");
+                                data.state = Console.ReadLine();
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter phne number");
+                                data.phoneNo = Console.ReadLine();
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter email");
+                                data.eMail = Console.ReadLine();
+                                break;
+                        }
+                        Console.WriteLine(address.firstname + "\n" + address.lastname + "\n" + address.address + "\n" + address.city + "\n" + address.state + "\n" + address.phoneNo + "\n" + address.postcode + "\n" + address.country + "\n" + address.eMail);
+                        
                     }
-                    Console.WriteLine(data.firstname + "\n" + data.lastname + "\n" + data.address + "\n" + data.city + "\n" + data.state + "\n" + data.phoneNo + "\n" + data.postcode + "\n" + data.country + "\n" + data.eMail);
 
                 }
 
-
             }
-
-
         }
     }
 }
